a592e1827f6cfad89d35b07564057349a11db650
Maftun Hashimli <maftunhashimli@gmail.com>
2022-08-10 00:56:33 +0300

    path variable changed request param

--- mjobb/src/main/java/com/mjobb/controller/JobAdvertisementController.java
+++ mjobb/src/main/java/com/mjobb/controller/JobAdvertisementController.java
@@ -6,8 +6,8 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,13 +19,14 @@ public class JobAdvertisementController {
     private final JobAdvertisementService jobAdvertisementService;
 
 
-    @GetMapping("add/favorite{jobId}")
-    public ResponseEntity<Void> addFavoriteJobForCurrentUser(@PathVariable @NotNull Long jobId) {
+    @GetMapping("add/favorite")
+    public ResponseEntity<Void> addFavoriteJobForCurrentUser(@RequestParam @NotNull Long jobId) {
         jobAdvertisementService.addFavoriteJobForCurrentUser(jobId);
         return ResponseEntity.accepted().build();
     }
-    @GetMapping("delete/favorite{jobId}")
-    public ResponseEntity<Void> deleteFavoriteJobForCurrentUser(@PathVariable @NotNull Long jobId) {
+
+    @GetMapping("delete/favorite")
+    public ResponseEntity<Void> deleteFavoriteJobForCurrentUser(@RequestParam @NotNull Long jobId) {
         jobAdvertisementService.deleteFavoriteJobForCurrentUser(jobId);
         return ResponseEntity.accepted().build();
     }
