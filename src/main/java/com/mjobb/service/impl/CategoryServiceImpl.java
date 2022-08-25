93dfc35d012657d23e3e95bc92159ad4411a8356
Maftun Hashimli <maftunhashimli@gmail.com>
2022-08-25 22:32:26 +0300

    category service impl add typical functions

--- src/main/java/com/mjobb/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/mjobb/service/impl/CategoryServiceImpl.java
@@ -3,13 +3,19 @@ package com.mjobb.service.impl;
 import com.mjobb.entity.Category;
 import com.mjobb.exception.WebServiceException;
 import com.mjobb.repository.CategoryRepository;
+import com.mjobb.repository.JobAdvertisementRepository;
 import com.mjobb.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,14 +24,22 @@ import java.util.Optional;
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private final JobAdvertisementRepository jobAdvertisementRepository;
     private static final int LIMIT = 8;
 
     @Override
-    public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+    public List<Category> getAllCategories(Optional <String> title) {
+
+        List<Category> categories = new ArrayList<>();
+        if (title.isEmpty())
+            categoryRepository.findAll().forEach(categories::add);
+        else
+            categoryRepository.findByNameContaining(String.valueOf(title)).forEach(categories::add);
+        if (categories.isEmpty()) {
+            return null;
+        }
+        return categories;
     }
-
-
 
     @Override
     public Optional<List<Category>> getPopularCategories() {
@@ -34,7 +48,14 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public void saveTag(Category category) {
+    public Category getCategoriesById(Long id){
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new WebServiceException("Not found Tag with id = " + id));
+        return category;
+    }
+
+    @Override
+    public void saveCategory(Category category) {
         categoryRepository.save(category);
     }
 
