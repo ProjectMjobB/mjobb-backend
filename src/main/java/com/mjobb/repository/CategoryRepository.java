09139b56992f86e291308d487aa96259feea4de4
Maftun Hashimli <maftunhashimli@gmail.com>
2022-08-25 22:32:09 +0300

    category repository add typical functions

--- src/main/java/com/mjobb/repository/CategoryRepository.java
+++ src/main/java/com/mjobb/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.mjobb.repository;
 import com.mjobb.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findByNameContaining(String title);
+
 }
