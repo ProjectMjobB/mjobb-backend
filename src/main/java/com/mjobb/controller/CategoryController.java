f6310f18b212670cb9cd4b0e7598fc573a1e7429
Maftun Hashimli <maftunhashimli@gmail.com>
2022-08-25 22:32:01 +0300

    category controller add typical functions

--- src/main/java/com/mjobb/controller/CategoryController.java
+++ src/main/java/com/mjobb/controller/CategoryController.java
@@ -1,10 +1,10 @@
 package com.mjobb.controller;
 
 import com.mjobb.entity.Category;
-import com.mjobb.entity.JobAdvertisement;
 import com.mjobb.service.CategoryService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
@@ -24,15 +24,21 @@ public class CategoryController {
 
 
     @GetMapping("/all")
-    public ResponseEntity<List<Category>> allCategories() {
-        return ResponseEntity.ok(categoryService.getAllCategories());
+    public ResponseEntity<List<Category>> getAllCategories(@RequestParam(required = false) String title) {
+
+        return new ResponseEntity<>(categoryService.getAllCategories(Optional.ofNullable(title)), HttpStatus.OK);
     }
 
     @PostMapping("/save")
     @Secured({"ROLE_ADMIN"})
     public ResponseEntity<Category> saveCategory(@RequestBody Category category) {
-        categoryService.saveTag(category);
+        categoryService.saveCategory(category);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoriesById(@PathVariable(value = "id") Long id) {
+        return new ResponseEntity<>(categoryService.getCategoriesById(id), HttpStatus.OK);
     }
 
     @PostMapping("/delete")
@@ -42,13 +48,9 @@ public class CategoryController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/get-by-id")
-    public ResponseEntity<?> getJobsFromCategory(@RequestParam Long id) {
-        Category category = categoryService.getCategoryById(id);
-        return ResponseEntity.ok(category.getJobAdvertisements());
-    }
     @GetMapping("/popular")
     public ResponseEntity<?> getPopularCategories(){
         return ResponseEntity.ok(categoryService.getPopularCategories());
     }
+
 }
