a7846f7795aee67189a7c5aa867bca66d91dbb12
Maftun Hashimli <maftunhashimli@gmail.com>
2022-08-25 22:32:59 +0300

    comment repository typical functions

--- src/main/java/com/mjobb/repository/CommentRepository.java
+++ src/main/java/com/mjobb/repository/CommentRepository.java
@@ -2,10 +2,18 @@ package com.mjobb.repository;
 
 import com.mjobb.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> getAllByAcceptedAndRejected(boolean accepted, boolean rejected);
+    List<Comment> findByToUserId(Long toUserId);
+    List<Comment> findByFromUserId(Long fromUserId);
+    @Transactional
+    void deleteByFromUserId(long toUserId);
+
+    @Transactional
+    void deleteByToUserId(long fromUserId);
 }
