1ee21b3ac3ed972377192cbf49e7dc465b6ab43d
Maftun Hashimli <maftunhashimli@gmail.com>
2022-08-25 22:32:17 +0300

    category service add typical functions

--- src/main/java/com/mjobb/service/CategoryService.java
+++ src/main/java/com/mjobb/service/CategoryService.java
@@ -3,19 +3,23 @@ package com.mjobb.service;
 import com.mjobb.entity.Category;
 import com.mjobb.entity.Tag;
 import com.mjobb.repository.CategoryRepository;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CategoryService {
-    List<Category> getAllCategories();
+    List<Category> getAllCategories(Optional <String> title);
 
-    void saveTag(Category category);
+    void saveCategory(Category category);
 
     void deleteCategory(Category category);
 
     Category getCategoryById(Long id);
 
      Optional<List<Category>> getPopularCategories();
+
+    Category getCategoriesById(Long id);
+
 
 }
