29c93c62ee8298f6fecd9422bc9d0e5414008915
Maftun Hashimli <maftunhashimli@gmail.com>
2022-08-25 22:33:57 +0300

    job adv repository typical functions

--- src/main/java/com/mjobb/repository/JobAdvertisementRepository.java
+++ src/main/java/com/mjobb/repository/JobAdvertisementRepository.java
@@ -1,7 +1,9 @@
 package com.mjobb.repository;
 
 import com.mjobb.entity.JobAdvertisement;
+import com.mjobb.entity.JobType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
@@ -10,6 +12,16 @@ public interface JobAdvertisementRepository extends JpaRepository<JobAdvertiseme
 
     List<JobAdvertisement> findAllByAccepted(boolean accepted);
     List<JobAdvertisement> findJobAdvertisementByTagsId(Long tagId);
+    List<JobAdvertisement> findByCategoryId(Long categoryId);
 
-    List<JobAdvertisement> findByTitleContaining(String title);
+    List<JobAdvertisement> findJobAdvertisementByLanguagesId(Long languageId);
+
+    List<JobAdvertisement> findByJobTypeId(Long postId);
+
+    @Transactional
+    void deleteByJobTypeId(long tutorialId);
+    @Transactional
+    void deleteByCategoryId(long categoryId);
+
+
 }
