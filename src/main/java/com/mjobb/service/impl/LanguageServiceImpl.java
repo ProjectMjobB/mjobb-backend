3c5023455d88be11ff3b4e3257d47e6ed1cd5e8c
Maftun Hashimli <maftunhashimli@gmail.com>
2022-08-25 22:35:58 +0300

    set language service impl

--- src/main/java/com/mjobb/service/impl/LanguageServiceImpl.java
+++ src/main/java/com/mjobb/service/impl/LanguageServiceImpl.java
@@ -1,12 +1,14 @@
 package com.mjobb.service.impl;
 
+import com.mjobb.entity.JobAdvertisement;
 import com.mjobb.entity.Language;
 import com.mjobb.exception.WebServiceException;
+import com.mjobb.repository.JobAdvertisementRepository;
 import com.mjobb.repository.LanguageRepository;
-import com.mjobb.repository.TagRepository;
 import com.mjobb.service.LanguageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -15,6 +17,7 @@ import java.util.List;
 public class LanguageServiceImpl implements LanguageService {
 
     private final LanguageRepository languageRepository;
+    private final JobAdvertisementRepository jobAdvertisementRepository;
 
 
     @Override
@@ -28,13 +31,65 @@ public class LanguageServiceImpl implements LanguageService {
     }
 
     @Override
-    public void deleteLanguage(Language language) {
-        languageRepository.delete(language);
+    public void deleteLanguage(long id) {
+        languageRepository.deleteById(id);
     }
 
     @Override
     public Language getLanguageById(Long id) {
         return languageRepository.findById(id).orElseThrow(() -> new WebServiceException("Language not found this id: " + id));
+    }
+    public List<Language> getAllLanguagesByJobAdvertisementId(Long jobId) {
+        if (!jobAdvertisementRepository.existsById(jobId)) {
+            throw new WebServiceException("Not found Job with id = " + jobId);
+        }
+        List<Language> languages = languageRepository.findLanguageByJobAdvertisementsId(jobId);
 
+        return languages;
+    }
+
+    public Language getLanguagesById(Long id) {
+        Language language = languageRepository.findById(id)
+                .orElseThrow(() -> new WebServiceException("Not found Language with id = " + id));
+
+        return language;
+    }
+
+    public List<JobAdvertisement> getAllJobsByLanguageId(Long languageId) {
+        if (!languageRepository.existsById(languageId)) {
+            throw new WebServiceException("Not found Language with id = " + languageId);
+        }
+        List<JobAdvertisement> jobs = jobAdvertisementRepository.findJobAdvertisementByLanguagesId(languageId);
+
+        return jobs;
+    }
+
+    public Language addLanguage(Long jobId, Language languageRequest) {
+        Language language = jobAdvertisementRepository.findById(jobId).map(job -> {
+            long langId = languageRequest.getId();
+
+            // tag is existed
+            if (langId != 0L) {
+                Language _language = languageRepository.findById(langId)
+                        .orElseThrow(() -> new WebServiceException("Not found Language with id = " + langId));
+                job.addLanguage(_language);
+                jobAdvertisementRepository.save(job);
+                return _language;
+            }
+
+            // add and create new Tag
+            job.addLanguage(languageRequest);
+            return languageRepository.save(languageRequest);
+        }).orElseThrow(() -> new WebServiceException("Not found Job with id = " + jobId));
+
+        return language;
+    }
+
+    public void deleteLanguageFromJob(@PathVariable(value = "jobId") Long jobId, @PathVariable(value = "languageId") Long languageId) {
+        JobAdvertisement job = jobAdvertisementRepository.findById(jobId)
+                .orElseThrow(() -> new WebServiceException("Not found Job with id = " + jobId));
+
+        job.removeLanguage(languageId);
+        jobAdvertisementRepository.save(job);
     }
 }
