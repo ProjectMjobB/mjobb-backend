c59adcb88338f3ec2f01f451665adb9bf6019d1d
Maftun Hashimli <maftunhashimli@gmail.com>
2022-08-25 22:35:42 +0300

    set language repository

--- src/main/java/com/mjobb/repository/LanguageRepository.java
+++ src/main/java/com/mjobb/repository/LanguageRepository.java
@@ -1,9 +1,14 @@
 package com.mjobb.repository;
 
+import com.mjobb.entity.JobAdvertisement;
 import com.mjobb.entity.Language;
 import com.mjobb.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LanguageRepository extends JpaRepository<Language, Long> {
     Language findByNameIgnoreCase(String name);
+    List<Language> findLanguageByJobAdvertisementsId(Long jobId);
+
 }
